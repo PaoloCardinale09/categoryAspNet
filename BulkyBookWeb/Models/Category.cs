d05180a35f154feef79a03fa644046271252a4dc
Shadow <Shadow@SHADOW-G8E9QBPI>
2023-11-29 16:12:10 +0100

    Aggiungere i file di progetto.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Il nome è obbligatorio")]
+        public string Name { get; set; }
+        [DisplayName("Display order")]
+        public string DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
